674926635be73655ca1271fd4655682348652dc6
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-03-15 12:27:32 +0530

    createBrowserRouter method added

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
 import { createRoot } from "react-dom/client";
 import Home from "./views/home";
 import Dashboard from "./views/dashboard";
-import Navbar from "./components/navbar";
 import About from "./views/about";
 import Contact from "./views/contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App(){
-    const path = window.location.pathname;
-    console.log(path);
-
-    if(path == "/"){
-        root.render(<Home />)
-    }
-    else if(path == "/navbar"){
-        root.render(<Navbar />)
-    }
-    else if (path == "/dashboard")
+const router = createBrowserRouter([
     {
-        root.render(<Dashboard />)
+        path : "/",
+        element : <Home/>
+    },
+    {
+        path : "./dashboard",
+        element: <Dashboard/>
+    },
+    {
+        path: "./about",
+        element : <About />
     }
-    else if (path == "/about"){
-        root.render(<About />)
+    {
+        path :  "./contact",
+        element: <Contact />
     }
-    else if (path == "/contact"){
-        root.render(<Contact />)
-    }
-    else{
-        root.render(<h1>404 Page not found</h1>)
-    }
-}
+])
 
 const root = createRoot(document.getElementById("root"));
 root.render(
